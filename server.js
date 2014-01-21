311d0c0df400257da27b764d6aa0648e32054cd1
jparias <jpa3211@gmail.com>
2014-01-20 22:11:41 -0500

    Se agregan principales las librerias y directorios

---
+++ server.js
@@ -0,0 +1,67 @@
+var express = require('express.io'),
+    swig = require('swig'),
+    _ = require('underscore'),
+    passport = require('passport');
+
+//Connect Express.io
+var server = express();
+server.http().io()
+
+//Connect Redis
+//var RedisStore = require('connect-redis')(express);
+
+var users = [];
+
+swig.setDefaults({
+  cache : false
+});
+
+// View engine
+server.engine('html', swig.renderFile );
+server.set('view engine', 'html');
+server.set('views', __dirname+'/minichat/views/');
+
+// Cookie and session support
+server.configure(function(){
+  server.use( express.static('./public') );
+
+  server.use( express.logger() );
+  server.use( express.cookieParser() );
+  server.use( express.bodyParser() );
+
+  server.use( express.session({
+    secret : "lolcatz",
+    // store  : new RedisStore({})
+    // store  : new RedisStore({
+    //  host : conf.redis.host,
+    //  port : conf.redis.port,
+    //  user : conf.redis.user,
+    //  pass : conf.redis.pass
+    // });  
+  }) );
+
+  server.use( passport.initialize() );
+  server.use( passport.session() );
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+server.get('/',function (req,res) {
+  res.render('home');
+});
+
+server.get('/messages/:message',function (req,res) {
+  res.send('Tu mensaje es '+req.params.message);
+});
+
+server.post('/log-in',function (req,res){
+  res.send('Envio el post');
+});
+
+server.listen(3000);
